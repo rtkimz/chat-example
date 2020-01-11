c474085c8069882a642a706bb784a6ff2bc69698
Robin <robinkimzey@gmail.com>
2020-01-10 22:25:24 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let app = require('express')()
+let http = require('http').createServer(app)
+
+app.get('/', function(req, res) {
+  res.send('<h1>Hello world!</h1>')
+})
+
+http.listen(3000, function() {
+  console.log('listening on *:3000')
+})
