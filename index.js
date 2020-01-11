a3c636124207b6f8f5915bca0d727a07c767ac05
Robin <robinkimzey@gmail.com>
2020-01-11 13:12:56 -0800

    route to index.html to display content

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-let app = require('express')()
-let http = require('http').createServer(app)
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {origins: ':'})
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello world!</h1>')
+  res.sendFile(__dirname, '/index.html')
+})
+
+io.on('connection', function(socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function() {
+    console.log('user disconnected')
+  })
 })
 
 http.listen(3000, function() {
